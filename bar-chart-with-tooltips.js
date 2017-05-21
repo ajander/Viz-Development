57221a2390d9c92d8a339c417a869ad1a321f09b
ajander <adrienne.j.anderson@gmail.com>
2017-05-21 13:46:08 -0500

    trying a simpler version

--- bar-chart-with-tooltips.js
+++ bar-chart-with-tooltips.js
@@ -8,9 +8,24 @@ var chart = d3.select('.chart')
 	.attr('width', width);
 
 // Define the div for the tooltip
-var div = d3.select(".chart").append("div")	
+var div = d3.select("body").append("div")	
     .attr("class", "tooltip")				
-    .style("opacity", 0);
+    .style("display", "none");
+
+function mouseover() {
+  div.style("display", "inline");
+}
+
+function mousemove() {
+  div
+      .text(d3.event.pageX + ", " + d3.event.pageY)
+      .style("left", (d3.event.pageX - 34) + "px")
+      .style("top", (d3.event.pageY - 12) + "px");
+}
+
+function mouseout() {
+  div.style("display", "none");
+}
 
 d3.tsv('./data.tsv', type, function(error, data) {
 	x.domain([0, d3.max(data, function(d) { return d.value; })]);
@@ -21,19 +36,9 @@ d3.tsv('./data.tsv', type, function(error, data) {
 		.data(data)
 	  .enter().append('g')
 	  	.attr('transform', function(d, i) { return 'translate(0,' + i * barHeight + ')'
-	  	.on("mouseover", function(d) {		
-            div.transition()		
-                .duration(200)		
-                .style("opacity", .9);		
-            div.html("This bar:<br/>" + d.name + "<br/>"  + d.value)
-            	.style("left", (d3.event.pageX) + "px")		
-                .style("top", (d3.event.pageY - 28) + "px");	
-            })					
-        .on("mouseout", function(d) {		
-            div.transition()		
-                .duration(500)		
-                .style("opacity", 0); 
-	  		});
+	  	.on("mouseover", mouseover)	
+	  	.on("mousemove", mousemove)				
+        .on("mouseout", mouseout);
 
 	bar.append('rect')
 	 	.attr('width', function(d) { return x(d.value); })
@@ -49,6 +54,6 @@ d3.tsv('./data.tsv', type, function(error, data) {
 function type(d) {
 	d.value = +d.value;	// coerce to number type
 	return d;
-};
+}
 
 
